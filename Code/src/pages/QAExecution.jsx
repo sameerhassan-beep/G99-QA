9c11b68e1e5c32df6d00ce41ce27a8744e5b2dd6
Sameer <sameer@diavel.local>
2026-03-10 08:16:33 +0530

    fix: update scrollbar styling in QAExecution.jsx

--- Code/src/pages/QAExecution.jsx
+++ Code/src/pages/QAExecution.jsx
@@ -1383,7 +1383,7 @@ function QAExecution() {
                           </Text>
                         </HStack>
 
-                        <Box overflowX="auto" maxHeight="600px" overflowY="auto" css={{
+                        <Box overflowX="auto" overflowY="auto" css={{
                           '&::-webkit-scrollbar': { width: '8px', height: '8px' },
                           '&::-webkit-scrollbar-track': { background: '#f1f1f1' },
                           '&::-webkit-scrollbar-thumb': { background: '#888', borderRadius: '4px' },
